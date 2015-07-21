8fb9ab357038d8169029c935ee513388df9b6cb0
Brenton Cheng <brenton@bfalling.net>
2015-07-21 04:20:12 -0700

    Consolidate app.js and main.js

--- js/motifspeedwriter.main.js
+++ js/motifspeedwriter.main.js
@@ -1,7 +1,33 @@
 var jQuery = jQuery || {};
+jQuery.noConflict();
 
 var MotifSpeedWriter = (function(my, $) {
   'use strict';
+
+  $(document).ready(function() {
+    $('#motif-text-clear-button').click(function(event) {
+      $('#motif-text').val('');
+      my.generateMotif('');
+      $('#motif-text').focus();
+      event.preventDefault();
+    });
+
+    // keyup fires multiple events, so just catch and process first one
+    $('#motif-text').keyup(function(event) {
+      var $this = $(this);
+      handleAutoTextEntry(event.which, $this);
+      my.generateMotif($this.val());
+      pushHistory($this.val());
+    });
+
+    $(window).bind("popstate", function() {
+        my.loadPage();
+    });
+
+    my.loadPage();
+  });
+
+  $(document).foundation();
 
   my.loadPage = function() {
     var match = location.search.match(/motif=([^&]*)/);
@@ -35,6 +61,35 @@ var MotifSpeedWriter = (function(my, $) {
     $('#motif-image-container').html('<img id="motif-image" src="' + canvasDataURL + '">');
 
   }; // generateMotif
+
+  var handleAutoTextEntry = function(key, textField) {
+    // Handle Motif staff entry
+    var val = textField.val();
+    var cursorPos = textField.caret();
+    var numDoubleBars = (val.match(/\|\|/g) || []).length;
+    var posDoubleBar = val.indexOf('||');
+    var verticalBarAscii = 220;
+    var leftParenAscii = 57;
+    // If entered the second bar of two-bar series and there are no other double bars in the text
+    if (key === verticalBarAscii && numDoubleBars === 1 && (cursorPos === posDoubleBar + 1 || cursorPos === posDoubleBar + 2)) {
+      // Only add one bar if for some reason there's a solo bar at the end
+      var stringToAdd = (posDoubleBar != val.length - 2 && val.charAt(val.length - 1) === '|') ? '|' : ' ||';
+      textField.val(textField.val() + stringToAdd);
+      textField.caret(cursorPos);
+      if (textField.val().charAt(cursorPos) !== '|') {
+        textField.caret(' ');
+      }
+    // Else if entered left paren, then auto-add right paren
+    } else if (key === leftParenAscii) {
+      textField.caret(')');
+      textField.caret(cursorPos);
+    }
+  };
+
+  var pushHistory = function(motifText) {
+    var motifParam = motifText ? '?motif=' + encodeURIComponent(motifText) : '';
+    window.history.pushState(null, 'Motif SpeedWriter | Laban Labs', location.pathname + motifParam);
+  };
 
   return my;
 
